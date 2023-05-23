c1b3fedb64131340ec784a20062620e29f269e89
Samuel <ssancheztoca@gmail.com>
2023-05-23 15:11:08 +0200

    falta arreglar docker

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,6 +6,6 @@ pip3 install -r requirements.txt
 
 # May as well do this too, while we're here.
 python3 manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 # en cmd de dockerfile
 
 exec "$@"
